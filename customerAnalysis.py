d451935a0c65b0754e26d4da65f39b3805ea61cd
annmerin <25018023+annmerin@users.noreply.github.com>
2021-12-02 22:03:28 -0800

    Merge remote-tracking branch 'origin/master'

--- customerAnalysis.py
+++ customerAnalysis.py
@@ -1,10 +1,18 @@
 import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+from matplotlib import pyplot as plt
+
+pd.set_option('display.max_columns',None)
+pd.set_option('display.max_rows',None)
 
 #cust_dataset = open("/Users/annmerinpeter/Documents/python/marketing_campaign.csv")
 #for x in cust_dataset:
 #    print(x)
 #cust_dataset.close()
 
-df = pd.read_csv("/Users/annmerinpeter/Documents/python/marketing_campaign.csv",delimiter='\t',nrows=20)
-print(df.columns)
-print(df)
+df = pd.read_csv("/Users/annmerinpeter/Documents/python/marketing_campaign.csv",delimiter='\t')
+print(df.head().truncate())
+
+plt.scatter(df['Education'],df['Marital_Status'],df['Income'],df['Kidhome'])
+plt.show()
